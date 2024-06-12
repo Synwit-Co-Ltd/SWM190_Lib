431f83f9e8e9b9e00b0ec46b317c5e664460367a
XIVN1987 <XIVN1987@163.com>
2024-06-12 17:40:57 +0800

    make clock switch easy and add demo

--- SWM190_StdPeriph_Driver/CSL/CMSIS/DeviceSupport/system_SWM190.h
+++ SWM190_StdPeriph_Driver/CSL/CMSIS/DeviceSupport/system_SWM190.h
@@ -6,6 +6,16 @@
 #endif
 
 
+#define SYS_CLK_24MHz		0	 	//0 内部高频24MHz RC振荡器
+#define SYS_CLK_48MHz		2		//2 内部高频48MHz RC振荡器
+#define SYS_CLK_XTAL		4		//4 外部晶体振荡器（2-30MHz）
+
+
+#define PLL_OUT_DIV8		0
+#define PLL_OUT_DIV4		1
+#define PLL_OUT_DIV2		2
+
+
 extern uint32_t SystemCoreClock;		// System Clock Frequency (Core Clock)
 extern uint32_t CyclesPerUs;			// Cycles per micro second
 
@@ -14,16 +24,16 @@ extern void SystemInit(void);
 
 extern void SystemCoreClockUpdate (void);
 
-extern void PLLInit(void);
-	 
+
 extern void switchTo24MHz(void);
 extern void switchTo3MHz(void);
 extern void switchTo48MHz(void);
 extern void switchTo6MHz(void);
 extern void switchToXTAL(uint32_t div8);
-extern void switchToPLL(uint32_t div8);
+extern void switchToPLL(uint32_t clksrc, uint32_t indiv, uint32_t fbdiv, uint32_t outdiv, uint32_t div8);
 extern void switchTo32KHz(void);
 extern void switchToXTAL_32K(void);
+
 
 #ifdef __cplusplus
 }
